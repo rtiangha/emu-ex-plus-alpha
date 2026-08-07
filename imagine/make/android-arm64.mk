ffb9f17edcad162478533a5b54c6c5d2a748ff80
Reg Tiangha <rtiangha@users.noreply.github.com>
2026-08-06 21:53:25 -0600

    Target Android 15 on Snapdragon 888

--- imagine/make/android-arm64.mk
+++ imagine/make/android-arm64.mk
@@ -4,10 +4,10 @@ ARCH := aarch64
 SUBARCH = arm64
 CHOST := aarch64-linux-android
 android_abi := arm64-v8a
-android_ndkSDK ?= 21
+android_ndkSDK ?= 35
 android_ndkArch := arm64
-clangTarget := aarch64-none-linux-android21
-CFLAGS_CODEGEN += -fPIC
+clangTarget := aarch64-none-linux-android35
+CFLAGS_CODEGEN += -fPIC -march=armv8.2-a+crypto+dotprod+fp16+rcpc
 LDFLAGS_SYSTEM += -Wl,-z,max-page-size=16384
 
 include $(buildSysPath)/android-gcc.mk
