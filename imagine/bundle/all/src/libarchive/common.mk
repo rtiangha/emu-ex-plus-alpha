1311c9164ca1284b8dd9a5ae57fc9273b3cf37f6
Reg Tiangha <rtiangha@users.noreply.github.com>
2026-08-07 16:16:24 -0600

    Revert "libarchive 3.8.9"
    This reverts commit 94c58600846525a73e268a1afbbf05442bf04893.

--- imagine/bundle/all/src/libarchive/common.mk
+++ imagine/bundle/all/src/libarchive/common.mk
@@ -4,7 +4,7 @@ endif
 
 include $(buildSysPath)/imagineSDKPath.mk
 
-libarchiveVer := 3.8.9
+libarchiveVer := 3.8.6
 libarchiveSrcDir := $(tempDir)/libarchive-$(libarchiveVer)
 libarchiveSrcArchive := libarchive-$(libarchiveVer).tar.xz
 
